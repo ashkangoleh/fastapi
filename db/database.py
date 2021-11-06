cda995c52ea68560e2f4a339ca559a06148e036c
ashkan <a.golehpour@arz.team>
2021-11-06 10:42:23 +0330

    new file:   .env_sample 	modified:   .gitignore 	modified:   db/database.py 	modified:   order_api/order_routes.py 	modified:   order_api/schema/order_schema.py 	new file:   requirments.txt 	modified:   thunder-collection_fastapi.json

--- db/database.py
+++ db/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from decouple import config
 
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_ADDRESS=config('DB_ADDRESS')
+DB_NAME = config('DB_NAME')
 
 engine = create_engine(
-    "postgresql://postgres:1@localhost/api", echo=True, convert_unicode=True
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}", echo=True, convert_unicode=True
 )
 
 Base = declarative_base()
