dfd9acc8c54912eba6ab84edf7ed770f2354992f
ashkan <ashkangoleh@gmail.com>
2021-11-13 17:21:52 +0330

    token and cookie added to settings

--- authentication_api/schema/auth_schema.py
+++ authentication_api/schema/auth_schema.py
@@ -28,6 +28,8 @@ class Settings(BaseModel):
     authjwt_secret_key: str = 'f00cc46cca11ba7fb31010c7435b8593267d8e973cf55e85d905083452246b20'
     authjwt_access_token:int =300
     authjwt_refresh_token:int =300 # 5min
+    authjwt_token_location: set = {"cookies"}
+    authjwt_cookie_csrf_protect: bool = False
 
 
 class LoginModel(BaseModel):
