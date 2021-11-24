d3425b11d4f03f3c81c24ed151c5c869778cc510
ashkangoleh <ashkangoleh@gmail.com>
2021-11-24 15:32:35 +0330

    uploadfile modified

--- uploader/upload_file.py
+++ uploader/upload_file.py
@@ -1,26 +1,24 @@
-import os
-from fastapi import File, UploadFile, APIRouter
+from fastapi import File, UploadFile, APIRouter, Depends
 from fastapi.exceptions import HTTPException
 from fastapi.responses import FileResponse
-import secrets
+from utils import AuthHandler
 from PIL import Image
+import os
 file_router = APIRouter()
 
 
-@file_router.post('/uploadfile', tags=['upload'],)
-async def upload_file(file: UploadFile = File(...)):
-    FILEPATH = "./media/"
+@file_router.post('/profile_image', tags=['upload'])
+async def upload_file(file: UploadFile = File(...), _user=Depends(AuthHandler.Token_requirement)):
+    FILEPATH = "./media/profile_image/"
     filename = file.filename
     extention = filename.split(".")[1]
 
     if extention not in ["jpg", "png"]:
         return False
 
-    # token_name = secrets.token_hex(10)+"."+extention
-    # generated_name = FILEPATH + token_name
-    generated_name = FILEPATH + filename
+    user_id = _user.get_raw_jwt()['detail']['id']
+    generated_name = FILEPATH + f"{user_id}.{extention}"
     file_content = await file.read()
-
     if not os.path.exists(generated_name):
         with open(generated_name, "wb") as file_object:
             file_object.write(file_content)
@@ -28,7 +26,7 @@ async def upload_file(file: UploadFile = File(...)):
         img = Image.open(generated_name)
         img = img.resize(size=(200, 200))
         img.save(generated_name)
-        
+
         file_object.close()
         return FileResponse(generated_name)
     else:
@@ -38,9 +36,3 @@ async def upload_file(file: UploadFile = File(...)):
         )
 
 
-@file_router.get('/uploadfile/{name}', tags=['upload'],)
-async def upload_file(name: str):
-    dir_path = "../media/"
-    file_location = f'{dir_path}/{name}'
-    if file_location:
-        return FileResponse(file_location)
