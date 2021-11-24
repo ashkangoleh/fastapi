545f921c9350f0ab64ea1fe37680bab69a5c7c1d
ashkangoleh <ashkangoleh@gmail.com>
2021-11-24 15:32:19 +0330

    model userProfile added

--- model/models.py
+++ model/models.py
@@ -7,10 +7,11 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
-    ForeignKey,DateTime,JSON
+    ForeignKey, DateTime, JSON
 )
 from sqlalchemy_utils import ChoiceType
 import datetime
+
 
 class User(Base):
     __tablename__ = "user"
@@ -26,6 +27,7 @@ class User(Base):
     orders = relationship('Order', back_populates="user")
     verification_code = relationship('CodeVerification', back_populates="user")
     users_log = relationship('UserLog', back_populates="user")
+    user_profile = relationship('UserProfile', back_populates="user")
 
     def __repr__(self):
         return f"<user {self.username}>"
@@ -62,22 +64,35 @@ class CodeVerification(Base):
     __tablename__ = "verification_code"
     id = Column(Integer, primary_key=True)
     code = Column(String(6), nullable=False)
-    validation = Column(Boolean,nullable=True,default=True)
-    expiration_time = Column(DateTime,default=datetime.datetime.now())
+    validation = Column(Boolean, nullable=True, default=True)
+    expiration_time = Column(DateTime, default=datetime.datetime.now())
     user_id = Column(Integer, ForeignKey("user.id"))
     user = relationship("User", back_populates='verification_code')
-    
+
     def __repr__(self):
         return f"<code for {self.id}>"
 
 
 class UserLog(Base):
     __tablename__ = "users_log"
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     user_log = Column(JSON)
-    login_datetime = Column(DateTime,default=datetime.datetime.now())
+    login_datetime = Column(DateTime, default=datetime.datetime.now())
     user_id = Column(Integer, ForeignKey("user.id"))
-    user = relationship("User",back_populates='users_log')
-    
+    user = relationship("User", back_populates='users_log')
+
     def __repr__(self):
         return f"<code for {self.id}>"
+
+
+class UserProfile(Base):
+    __tablename__ = "user_profile"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
+    address = Column(Text)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship('User', back_populates="user_profile")
+
+    def __repr__(self):
+        return f"<code for {self.id}"
