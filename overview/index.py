96f3bdba9177b278d04122d5bef35436f5db112f
ashkangoleh <ashkangoleh@gmail.com>
2021-12-01 14:29:37 +0330

    wrtc simple added

--- overview/index.py
+++ overview/index.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request,Header,Response
 from fastapi.responses import HTMLResponse, FileResponse
-
+import os
+from pathlib import Path
 from fastapi.templating import Jinja2Templates
 app_view = APIRouter()
 
 TEMPLATES = Jinja2Templates(directory="templates")
-
+CHUNK_SIZE = 1024*1024
+video_path = Path(os.path.dirname(os.path.dirname(__file__))+'/static/video.mp4')
 
 @app_view.get("/")
 async def read_index(request: Request):
@@ -15,3 +17,18 @@ async def read_index(request: Request):
         "overview": "this is jinja2 test for fastapi",
     }
     return TEMPLATES.TemplateResponse("index.html", context=context)
+
+@app_view.get("/video")
+async def video_endpoint(range: str = Header(None)):
+    start, end = range.replace("bytes=", "").split("-")
+    start = int(start)
+    end = int(end) if end else start + CHUNK_SIZE
+    with open(video_path, "rb") as video:
+        video.seek(start)
+        data = video.read(end - start)
+        filesize = str(video_path.stat().st_size)
+        headers = {
+            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+            'Accept-Ranges': 'bytes'
+        }
+        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
