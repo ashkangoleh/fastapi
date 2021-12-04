aed60a5718c1c52a2f8db5aa13357976973e0bca
ashkangoleh <ashkangoleh@gmail.com>
2021-12-04 10:58:55 +0330

    anti spam initialized

--- overview/index.py
+++ overview/index.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from fastapi import APIRouter, Request,Header,Response
+from fastapi.exceptions import HTTPException
 from fastapi.responses import HTMLResponse, FileResponse
 import os
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
+import threading   
 app_view = APIRouter()
 
 TEMPLATES = Jinja2Templates(directory="templates")
@@ -32,3 +35,29 @@ async def video_endpoint(range: str = Header(None)):
             'Accept-Ranges': 'bytes'
         }
         return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+    
+    
+import datetime
+counter = 0
+lock = threading.Lock()
+_2min = datetime.datetime.now()+datetime.timedelta(seconds=3)
+@app_view.get("/test")
+def do_something():
+    global counter
+    global _2min
+    now = datetime.datetime.now()
+    try:
+        with lock:
+            counter += 1
+            if counter >= 3:
+                raise HTTPException(
+                    status_code=400,
+                    detail="stop spam"
+                )
+    except Exception as e:
+        if now > _2min:
+             counter = 0
+        raise HTTPException(
+            status_code=400
+        )
+    return {"message": "Hello World"}
