960dbf15c49376e82b9ed0c17338c953dd75106d
ashkangoleh <ashkangoleh@gmail.com>
2021-12-15 17:30:52 +0330

    modified:   db/migrations/README 	modified:   db/migrations/env.py

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -1,32 +1,26 @@
-import os
-import sys
+from db import Base
 from logging.config import fileConfig
-
-from sqlalchemy import create_engine
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 from alembic import context
-from decouple import config
-from db.database import Base
-
-DB_USER = config('POSTGRES_USER')
-DB_PASSWORD = config('POSTGRES_PASSWORD')
-DB_ADDRESS=config('POSTGRES_ADDRESS')
-DB_NAME = config('POSTGRES_DB')
-
 config = context.config
-
 fileConfig(config.config_file_name)
-
-base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(base_dir)
-
-
 target_metadata = Base.metadata
 
-def get_url():
-    return f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}"
 
 def run_migrations_offline():
-    url = get_url()
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -37,30 +31,23 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
-def render_item(type_, obj, autogen_context):
-    if type_ == 'type' and isinstance(obj, sqlalchemy_utils.types.choice.ChoiceType):
-        autogen_context.imports.add("import sqlalchemy_utils")
-        col_type = "sqlalchemy_utils.types.choice.ChoiceType({})"
-        return col_type.format(obj.choices)
-    return False
 
 def run_migrations_online():
-    connectable = create_engine(get_url())
+    """Run migrations in 'online' mode.
 
-    def process_revision_directives(context, revision, directives):
-        if getattr(config.cmd_opts, 'autogenerate', False):
-            script = directives[0]
-            if script.upgrade_ops.is_empty():
-                directives[:] = []
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
 
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            render_item=render_item,
-            process_revision_directives=process_revision_directives,
-            # **current_app.extensions["migrate"].configure_args,
+            connection=connection, target_metadata=target_metadata
         )
 
         with context.begin_transaction():
