7f102d16c8e810e8dba473d4febaec7fcbde42fe
ashkangoleh <ashkangoleh@gmail.com>
2021-12-15 15:33:22 +0330

    runner files improved

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --workers 3 --log-level critical
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload --workers 3 --log-level critical --log-config ./log.ini
