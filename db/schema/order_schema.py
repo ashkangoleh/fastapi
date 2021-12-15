424059484dace656d1a7e249d79428818b958489
ashkangoleh <ashkangoleh@gmail.com>
2021-12-15 16:22:10 +0330

    order schema enum added

--- db/schema/order_schema.py
+++ db/schema/order_schema.py
@@ -1,11 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+from enum import Enum
+ORDER_SIZES = (
+    ("SMALL", "small"),
+    ("MEDIUM", "medium"),
+    ("LARGE", "large"),
+    ("EXTRA-LARGE", "extra-large"),
+)
+
+
+class status(str, Enum):
+    PENDING = "pending"
+    IN_TRANSIT = "in-transit"
+    DELIVERED = "delivered"
+
+
+class sizes(str, Enum):
+    SMALL = "small"
+    MEDIUM = "medium"
+    LARGE = "large"
+    EXTRA = "extra-large"
+
 
 class OrderModel(BaseModel):
     id: Optional[int]
     quantity: int
-    order_status: Optional[str] = "PENDING"
-    order_sizes: Optional[str] = "SMALL"
+    order_status: Optional[str] = Field(None, alias="Status")
+    order_sizes: Optional[str] = Field(None, alias="Sizes")
     user_id: Optional[int]
 
     class Config:
@@ -28,7 +49,6 @@ class OrderStatusModel(BaseModel):
                 "order_status": "PENDING"
             }
         }
-
 
 
 class OrderTest(BaseModel):
